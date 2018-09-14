bee4ca87b64c13a07bc5280de161328e418a85a9
Evan.Peng <Evan.Peng@orchestrallinc.cn>
2018-09-14 16:20:59 +0800

    帮助反馈

--- src/components/teach/helpAndSupport.vue
+++ src/components/teach/helpAndSupport.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="helpAndSupport">
-    hvvvvv
+    <div style="font-size: 18px;margin: 20px 30px;color: blue">使用概述</div>
+    <div style="margin:30px;height: 300px;background: #FFEBE6;"></div>
+    <div style="font-size: 18px;margin: 20px 30px;color: blue">充值说明</div>
+    <div style="margin:30px;height: 200px;background: #FFEBE6"></div>
+    <div style="font-size: 18px;margin: 20px 30px;color: blue">
+      <span>意见反馈</span>
+      <span style="margin-left:60px">关于&nbsp;1+X直播系统</span>
+    </div>
   </div>
 </template>
 
